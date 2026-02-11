edce2e7300d3bd513e23b8ff70646611a6d33996
Simon Stensgaard <STcena1@hotmail.com>
2026-02-11 20:41:17 +0100

    Added update_centroid functionality to CachePartition-class

--- rust/src/cache/cache_partition.rs
+++ rust/src/cache/cache_partition.rs
@@ -13,8 +13,45 @@ pub struct CachePartition<const D: usize> {
     entry_count: usize,
 
     /// K-means centroids representing the partition's vector clusters (Mutable).
-    centroid: Vec<[f32; D]>,
+    centroid: Option<Vec<[f32; D]>>,
 
     /// Internal storage for vector entries (Mutable).
     entries: Vec<VectorEntry<D>>,
+}
+
+impl<const D: usize> CachePartition<D> {
+    pub fn new(partition_id: u64, max_entries: usize) -> Self {
+        Self {
+            partition_id,
+            max_entries,
+            entry_count: 0,
+            centroid: None,
+            entries: Vec::new(),
+        }
+    }
+
+    pub fn update_centroid(&mut self){
+        let count = self.entry_count as f32;
+        if count == 0.0 {
+            self.centroid = None;
+            return;
+        }
+
+        let mut mean = [0.0f32; D];
+
+        for entry in &self.entries {
+            for index in 0..D {
+                mean[index] += entry.vector[index];
+            }
+        };
+
+        let new_centroid = mean.map(|x| x / count);
+
+        self.centroid = Some(vec![new_centroid]);
+        
+    }
+
+    fn is_full(&self) -> bool {
+        self.entry_count >= self.max_entries
+    }
 }
