21d54ab806d7000ce08ba097d89cad13846ae8d3
Simon Stensgaard <STcena1@hotmail.com>
2026-02-23 21:56:19 +0100

    Started working on Vector Arena storage

---
+++ crates/tectonic-core/src/storage/arena.rs
@@ -0,0 +1,35 @@
+use std::collections::VecDeque;
+
+
+
+#[derive(Debug, Clone)]
+pub struct VectorArena<const D: usize> {
+    next_index: usize,
+    free_list: VecDeque<usize>,
+    capacity: usize,
+    size: usize,
+    arena: [f32; D]
+}
+
+impl<const D: usize> VectorArena<D> {
+
+    fn insert(&mut self, value: f32) -> bool {
+        todo!()
+    }
+
+    fn generate_id(&self) -> usize {
+        todo!()
+    }
+
+    pub fn load_factor(&self) -> f32 {
+        (self.size as f32 / self.capacity as f32) * 100.0
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.size > self.capacity
+    }
+}
