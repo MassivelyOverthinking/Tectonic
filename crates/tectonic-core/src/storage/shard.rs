b2924ac6368a22d36a1322367f1bb08a50265a65
Simon Stensgaard <STcena1@hotmail.com>
2026-02-25 22:13:01 +0100

    working on Internal cache repo initialization

--- crates/tectonic-core/src/storage/shard.rs
+++ crates/tectonic-core/src/storage/shard.rs
@@ -2,16 +2,26 @@
 // IMPORTS AND MODULES
 // ============================================================
 
+use crate::storage::location::Location;
+
 // ============================================================
 // INTERNAL SHARDS (MULTITHREADING)
 // ============================================================
 
 pub struct CacheShard {
     pub shard_id: usize,
+    pub capacity: usize,
+    pub size: usize,
+    pub location_storage: Vec<Option<Location>>
 }
 
-impl CachePartition {
-    pub fn with_capacity(capacity: usize, num_shards: usize) -> Self {
-        
+impl CacheShard {
+    pub fn with_capacity(id: usize, capacity: usize) -> Self {
+        Self { 
+            shard_id: id,
+            capacity: capacity,
+            size: 0,
+            location_storage: vec![None; capacity],
+        }
     }
 }
