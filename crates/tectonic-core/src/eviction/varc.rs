ec75ae8415aff88894aeda2aca428d62055dc0c5
Simon Stensgaard <STcena1@hotmail.com>
2026-04-24 20:16:47 +0200

    Update: Added general helper-methods to VARC eviciton policy

--- crates/tectonic-core/src/eviction/varc.rs
+++ crates/tectonic-core/src/eviction/varc.rs
@@ -41,3 +41,75 @@ pub struct VARC {
     capacity: usize,
     pivot: usize,
 }
+
+impl Default for VARC {
+    #[inline]
+    fn default() -> Self {
+        Self::with_capacity(100)
+    }
+}
+
+impl VARC {
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[inline]
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self { 
+            t1: TectonicDoublyLinkedList::with_capacity(capacity),
+            t2: TectonicDoublyLinkedList::with_capacity(capacity), 
+            b1: TectonicDoublyLinkedList::with_capacity(capacity), 
+            b2: TectonicDoublyLinkedList::with_capacity(capacity), 
+            index_map: HashMap::with_capacity(capacity.saturating_mul(2)), 
+            capacity, 
+            pivot: 0 
+        }
+    }
+
+    #[inline]
+    pub fn resident_len(&self) -> usize {
+        self.t1.len() + self.t2.len()
+    }
+
+    #[inline]
+    pub fn ghost_len(&self) -> usize {
+        self.b1.len() + self.b2.len()
+    }
+
+    #[inline]
+    pub fn list_len(&self) -> usize {
+        self.resident_len() + self.ghost_len()
+    }
+
+    #[inline]
+    pub fn list(&self, list_type: ArcType) -> &TectonicDoublyLinkedList {
+        match list_type {
+            ArcType::T1 => &self.t1,
+            ArcType::T2 => &self.t2,
+            ArcType::B1 => &self.b1,
+            ArcType::B2 => &self.b2,
+        }
+    }
+
+    #[inline]
+    pub fn list_mut(&mut self, list_type: ArcType) -> &mut TectonicDoublyLinkedList {
+        match list_type {
+            ArcType::T1 => &mut self.t1,
+            ArcType::T2 => &mut self.t2,
+            ArcType::B1 => &mut self.b1,
+            ArcType::B2 => &mut self.b2,
+        }
+    }
+
+    #[inline]
+    pub fn is_resident(list_type: ArcType) -> bool {
+        matches!(list_type, ArcType::T1 | ArcType::T2)
+    }
+
+    #[inline]
+    pub fn is_ghost(list_type: ArcType) -> bool {
+        matches!(list_type, ArcType::B1 | ArcType::B2)
+    }
+}
