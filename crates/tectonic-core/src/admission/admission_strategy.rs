0c5e995af5b8b89735f6f22aaad73cafa641c3d5
Simon Stensgaard <STcena1@hotmail.com>
2026-04-25 19:08:19 +0200

    New: Updated basic AdmissionStrategy struct and functionality with new features

--- crates/tectonic-core/src/admission/admission_strategy.rs
+++ crates/tectonic-core/src/admission/admission_strategy.rs
@@ -7,16 +7,27 @@
 // ADMISSION STRATEGIES
 // ============================================================
 
+use crate::utility::utils::UniqueID;
+
 
 #[derive(Debug, Clone, Copy)]
 #[allow(dead_code)]
 pub enum Admission {
     Always,
     Threshold,
-    Priority,
-    TinyLRU,
-    Weighted,
+    TwoHit,
+    TinyLFU,
+    WeightedTinyLFU,
     SemAware,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[allow(dead_code)]
+pub struct AdmissionCandidate {
+    id: UniqueID,
+    score: Option<f32>,
+    cost: usize,
+    priority: u32,
 }
 
 #[allow(dead_code)]
@@ -27,5 +38,5 @@ pub trait AdmissionStrategy {
 
     fn on_remove(&mut self);
     
-    fn should_admit(&mut self) -> bool;
+    fn should_admit(&mut self, candidate: &AdmissionCandidate) -> bool;
 }
